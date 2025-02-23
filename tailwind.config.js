d0a6a894b00c8b79587de9379be139d3cc340f8b
LuthfiAditya01 <luthfi2004aditya@gmail.com>
2025-02-23 15:08:43 +0700

    Initial Commit From Local

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./**/*.{html,js}", // Update this path to match your project structure
+    "./*.html" // Add other paths if needed
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        DynaPuff: ['DynaPuff', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
