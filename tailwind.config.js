260c06ae72a68519f5aefb93f254a419803b53f6
LuthfiAditya01 <luthfi2004aditya@gmail.com>
2025-02-27 02:55:37 +0700

    Adding Hero Section in index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // "./**/*.{html,js}", // Update this path to match your project structure
-    "./*.html" // Add other paths if needed
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        DynaPuff: ['DynaPuff', 'sans-serif'],
-      }
+    content: [
+        // "./**/*.{html,js}", // Update this path to match your project structure
+        './*.html', // Add other paths if needed
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                DynaPuff: ['DynaPuff', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
